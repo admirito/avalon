d82bd548f10541cce60891bd516a7b37ad8aef26
Mohammadreza Moghaddas <moghaddas@soc1.ir>
2022-11-20 11:38:21 +0330

    a line width corrected.

--- avalon/mediums.py
+++ avalon/mediums.py
@@ -232,7 +232,8 @@ class KafkaMedia(BaseMedia):
         # producer have to be created per process
         if not self._producer:
             self._producer = kafka.KafkaProducer(
-                bootstrap_servers=self._options["bootstrap_servers"].split(","))
+                bootstrap_servers=self._options["bootstrap_servers"].split(",")
+            )
         self._producer.send(topic=self._topic, value=batch.encode("utf-8"))
         if self.force_flush:
             self._producer.flush(3)
