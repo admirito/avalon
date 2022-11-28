4ae1a237050d1fae5928f875054b14648f8489ac
Mohammad Razavi <mrazavi64@gmail.com>
2022-11-28 11:33:14 +0330

    Refactor models package and add support for extensions

--- avalon/auxiliary.py
+++ avalon/auxiliary.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 import fcntl
+import fnmatch
+import importlib
 import os
+import re
 import signal
+
 
 class DirectoryNotifier:
     """
@@ -56,3 +60,39 @@ class DirectoryNotifier:
         if not signal.sigtimedwait(
             [self.__class__.sig, signal.SIGTERM, signal.SIGINT], self.timeout):
             self(-1, None)
+
+
+def importall(package, pattern="*.py"):
+    """
+    Given a python package object and a filename pattern, all the
+    modules inside the package will be imported and returned as a
+    list.
+
+    Namespace packages are supported by this mehtod but modules
+    requiring import hooks
+    (https://docs.python.org/3/reference/import.html#import-hooks) are
+    not supported and only normal files with valid python module
+    identifiers and ending with .py suffix will be imported.
+    """
+    VALID_MODULE_NAME = re.compile(r"[_a-z]\w*\.py$", re.IGNORECASE)
+
+    result = []
+
+    for top_dir in package.__path__:
+        try:
+            for path in sorted(os.listdir(top_dir)):
+                full_path = os.path.join(top_dir, path)
+                if (os.path.isfile(full_path) and
+                        fnmatch.fnmatch(path, pattern) and
+                        VALID_MODULE_NAME.match(path)):
+                    import_name = f"{package.__name__}.{path[:-3]}"
+                    try:
+                        module = importlib.import_module(import_name)
+                    except ImportError:
+                        pass
+                    else:
+                        result.append(module)
+        except OSError:
+            pass
+
+    return result
